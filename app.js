11c91942109f60fdc3b204f0bb78929faefa627b
Ben West <benwest0291@outlook.com>
2021-10-06 15:15:29 +0100

    added comments in JS file as a reminder

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+// Grabbing hold of the id of results
 const results = document.querySelector("#results");
 
+// funtion called searchMovies that can be called
+// grabbing api and itoration and displaying on DOM
 const searchMovies = (query) => {
   fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
     .then((response) => response.json())
@@ -8,20 +11,23 @@ const searchMovies = (query) => {
         const movie = `<li class="list-inline-item">
           <img src="${result.Poster}" alt="">
           <h4>${result.Title}</h4>
+          <p>Year Of Release: ${result.Year}</p>
         </li>`;
         results.insertAdjacentHTML("beforeend", movie);
       });
     });
 };
 
-
+// grabbing the main form id
 const form = document.querySelector("#search-movies");
-
+// adding a event listener of submit
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // <-- to prevent <form>'s native behaviour of refreshing after submit
-  const input = event.currentTarget.querySelector(".form-control");
-  results.innerHTML = "";
-  searchMovies(input.value);
+  event.preventDefault(); // <-- to prevent <form>'s natral behaviour of refreshing after submit
+  const input = event.currentTarget.querySelector(".form-control"); // <-- grabbing the input of user from class form-control TO search API
+  results.innerHTML = ""; // <-- Stop added the results to the end of each search
+  searchMovies(input.value); // <-- calling search movies and giving it an arguement of input value
 });
 
+
+// Calling searchMovie Function and displaying name of movie as default
 searchMovies("movie");
