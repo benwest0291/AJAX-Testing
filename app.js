4f8c630dc4c423780c8fca9ce5fac7d7c819f75d
Ben West <benwest0291@outlook.com>
2021-10-06 13:40:02 +0100

    added style to netflix search clone

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const results = document.querySelector("#results");
 
-fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
-  .then((response) => response.json())
-  .then((data) => {
-    data.Search.forEach((result) => {
-      const movieTag = `<li class="list-inline-item">
-        <img src="${result.Poster}" alt="">
-        <p>${result.Title}</p>
-      </li>`;
-      results.insertAdjacentHTML("beforeend", movieTag);
+const searchMovies = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        const movie = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+        </li>`;
+        results.insertAdjacentHTML("beforeend", movie);
+      });
     });
-  });
+};
+
+
+const form = document.querySelector("#search-movies");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // <-- to prevent <form>'s native behaviour
+  const input = event.currentTarget.querySelector(".form-control");
+  results.innerHTML = "";
+  searchMovies(input.value);
+});
